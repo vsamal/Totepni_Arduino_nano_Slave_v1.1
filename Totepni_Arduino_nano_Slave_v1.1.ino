c0ad41ad5fefa2b99090cb449e8f10799a06f87d
vsamal <samal@ipf.cz>
2017-02-03 14:37:03 +0100

    I2C communication

--- Totepni_Arduino_nano_Slave_v1.1.ino
+++ Totepni_Arduino_nano_Slave_v1.1.ino
@@ -1,56 +1,45 @@
-#include "SPI.h"
+//#include "SPI.h"
 #include "Wire.h"
-#include "UIPEthernet.h"
 #include "SoftwareSerial.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
-#include "AM2320.h"
-#include "avr/wdt.h"
+//#include "avr/wdt.h"
 
+
+// software serial PINs set up
+int sw_rx = 2;
+int sw_tx = 3;
+
+// I2C buffer
+int prijmuto[27];
+float prijmuto_dec;
+String prijmuto_text;
+
+
+// SoftwareSerial SIM900(sw_rx, sw_tx); //RX,TX
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-
-
-
-AM2320 th;
-
-float teplota;
-float vlhkost;
 
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
 
-
-
-EthernetClient client;
-signed long next;
-char read_buffer;
-boolean nalez = false;
-
-// the dns server ip
-IPAddress dnServer(193, 85, 1, 12);
-// the router's gateway address:
-IPAddress gateway(192, 168, 1, 1);
-// the subnet:
-IPAddress subnet(255, 255, 255, 0);
-//the IP address is dependent on your network
-IPAddress ip(192, 168, 1, 15);
-
-uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
-char server[] = "topeni.cz"; //server, kam se pripojujeme
-
-
-
-
 void setup()   {                
 
-  wdt_enable(WDTO_8S);
+  // wdt_enable(WDTO_8S);
+
+
+  Wire.begin(100);
+  Wire.onReceive(priPrijmu);
+  Wire.onRequest(odeslatData);
+
   
   Serial.begin(9600);
+  // SIM900.begin(9600); 
+
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
@@ -59,32 +48,38 @@ void setup()   {
   // Clear the buffer.
   display.clearDisplay();
 
-  Ethernet.begin(mac, ip, dnServer, gateway, subnet);
-
-  next = 0;
 
 }
 
 
 void loop() {
   
-  readTemp();
+  // readTemp();
   // readGET();
-  showtext("Obyvak", "teplota");
-  showtext("Loznice", "teplota");
-  showtext("Pokojicek", "teplota");
-  showtext("Puda", "teplota");
-  showtext("Venku", "teplota");
+  showtext("Kuchyn + K", "teplota", 8);
+  showtext("Pokoj puda", "teplota", 12);
+  showtext("Obyvak", "teplota", 14);
+  showtext("Loznice", "teplota", 16);
+  showtext("Venku", "teplota", 18);
 
 
-  wdt_reset();
+  clr_wdt();
 
 }
 
 
+void clr_wdt() {
+
+      // clr_wdt();
+  
+}
 
 
-void showtext(String radek1, String radek2) {
+
+void showtext(String radek1, String radek2, int radek3) {
+  int teplota = 1;
+  int vlhkost = 2;
+  
   display.setTextSize(2);
   display.setTextColor(WHITE, BLACK);
   display.setCursor(0,0);
@@ -92,10 +87,10 @@ void showtext(String radek1, String radek2) {
 
   display.println(radek1);
   if(radek2 == "teplota"){
-    display.print(teplota);
+    display.print(prijmuto[radek3]);
     display.print(" ");
     display.setTextSize(1);
-    display.print(vlhkost);
+    display.print(prijmuto[10]);
     display.println("%");
   }else{
     display.println(radek2);
@@ -104,96 +99,142 @@ void showtext(String radek1, String radek2) {
   delay(2100);
   display.clearDisplay();
 
-  wdt_reset();
+  clr_wdt();
  
 }
 
 
 
-void readTemp() {
-  switch(th.Read()) {
-    case 2:
-      teplota = 99;
-      break;
-    case 1:
-      teplota = -99;
-      break;
-    case 0:
-      vlhkost = th.h;
-      teplota = th.t;
-      break;
-  }
 
-  delay(200);
-  wdt_reset();
+void getI2Cvals(int val_int, int val_decimal){
+  
+        float val_dec = val_int + (val_decimal / 10);
+        return val_dec;  
 }
 
 
 
+void delayWDT(int delaysec = 30){
+            clr_wdt();
+       
+            int waiting_call = 0;
+            while(waiting_call <= delaysec){
+                delay (1000);
+                clr_wdt();
+                waiting_call += 1;
+            }            
+            clr_wdt();
+}
 
-void readGET() {
 
-  if (((signed long)(millis() - next)) > 0)
-    {
-      next = millis() + 11000;
 
-      Serial.println("Connecting to server");
-      
-      if (client.connect(server,80)){
-
-          Serial.println("Connected");
-          
-          String myURL = "GET /topeni/topeni.php?time=";
-          myURL += millis();
-
-          Serial.println(myURL);
-          
-          client.print(myURL);
-          client.println(" HTTP/1.0");
-          client.println("Host: samal.es");
-          client.println("Connection: close");
-          client.println();
-
-          nalez = false;
-          
-         while(client.connected()) {
-          if(client.available()) {
-              char read_buffer = client.read();
-              
-              // Serial.print(read_buffer); //vypise prijata data
+void priPrijmu(int b){
+    int prijmuto_counter = 0;
+    int read_data;
     
-              
-              if (read_buffer == '>'){nalez = false;}
-               
-              if (nalez){
-                // Serial.print("->");
-                Serial.print(read_buffer); //vypise prijata data
-                // Serial.print("<-");
-              }
-
-              if (read_buffer == '<'){nalez = true;}
-                       
-          }
-        }
+    Serial.println("--- Start Read ---");
     
-         Serial.println();
-         Serial.println("Odpojuji.");
-         delay(10);
-         client.flush();
-         delay(10);
-         Serial.println();
-         client.stop();   
-         
-       }else{
-        
-        Serial.println("Connect failed");
-
-       }
-        
+    while(Wire.available() > 0){
+        read_data = Wire.read();
+        prijmuto[prijmuto_counter] = read_data;
+        //Serial.println(Wire.read());
+        Serial.print("prijmuto  [ ");
+        Serial.print(prijmuto_counter);
+        Serial.print(" ]  = > ");
+        Serial.println(prijmuto[prijmuto_counter]);
+        prijmuto_counter++;
     }
 
-    wdt_reset();
+    Serial.println("--- End Read ---");
+
+    //prijmuto_dec = getI2Cvals(prijmuto[0], prijmuto[1]);
+
+    /*  
+      prijmuto_text.toInt();
+      
+      https://www.arduino.cc/en/Reference/CharacterAnalysis
+      
+
+    */
 }
+
+
+
+void odeslatData() {
+  Serial.flush();
+  // Wire.write(prijmuto[0] + "." + prijmuto[1], 3);
+  int k = 1;
+  while(k <= 19){
+    Wire.write(k);
+    k++;
+  }
+  
+  // Wire.write(3);
+  clr_wdt();
+}
+
+
+/*
+
+// alarm zavola na mobil
+void setAlarm(){
+            Serial.println("Alarm");                   
+            
+            //makeCall("604833891");
+            //makeCall("605906254");
+            //makeCall("737226659");
+            //sendSMS("737226659");
+            //makeCall("737226659");
+            
+}
+
+
+
+void makeCall(char callnumber[]){
+            clr_wdt();
+            
+            SIM900.print("ATD");
+            SIM900.print(callnumber);
+            SIM900.println(";");
+            delay(100);            
+             
+            while (SIM900.available()){
+            s = SIM900.read();
+              Serial.print(s);
+            }  
+        
+            delayWDT(29);
+                       
+            SIM900.println("ATH");                      
+            delay (1000);
+            clr_wdt();
+}
+
+
+void sendSMS(char smsnumber[]){
+          SIM900.println("AT+CMGF=1");  // AT command na odeslani SMS zpravy
+          delay(100);
+          SIM900.print("AT+CMGS=\"");  // cislo prijemce
+          SIM900.print(smsnumber);  // cislo prijemce
+          SIM900.println("\"");  // cislo prijemce
+          delay(100);
+          SIM900.println("Alarm send.");  // zprava k odeslani
+          SIM900.print("Status: ");  // zprava k odeslani
+          SIM900.print(rele_modul[1]);  // zprava k odeslani
+          SIM900.print(rele_modul[2]);  // zprava k odeslani
+          SIM900.print(rele_modul[3]);  // zprava k odeslani
+          SIM900.println(rele_modul[4]);  // zprava k odeslani
+          SIM900.print(" is set.");  // zprava k odeslani
+          delay(100);
+          SIM900.println((char)26);  //  AT command znak a ^Z, ASCII code 26 pro konec SMS zpravy
+          delay(100); 
+          SIM900.println();
+          delayWDT(11);  // pokej na odesln SMS
+          //SIM900power();  // vypni GSM module
+          clr_wdt();
+        }
+
+*/
 
 
 
